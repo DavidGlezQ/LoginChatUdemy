3d6e6bc8fa1ab35f854dbd30b35aa08bb456b6b2
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2020-06-18 01:51:07 -0500

    Update 18/16/2020
    Creacion de los archivos bubble para el fragmentChat.
    Se agrega iconos de material design para los editText.
    Se agrega los corners junto a los ripple de lso botones.

--- app/src/main/java/com/example/loginchatudemy/activities/MainActivity.kt
+++ app/src/main/java/com/example/loginchatudemy/activities/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         setUpBottomNavigationBar()
         SignOut()
         //FIN LLAMADO DE FUNCIONES
+        mAuth.signOut()
 
 
     }
---
+++ app/src/main/java/com/example/loginchatudemy/models/message.kt
@@ -0,0 +1,5 @@
+package com.example.loginchatudemy.models
+
+import java.util.*
+
+data class Message(val authorId: String, val message: String, val profileImageUrl: String, val sendAt: Date)
